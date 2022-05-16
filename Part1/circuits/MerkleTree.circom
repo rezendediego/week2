49f2ab7f488fd5cbb3f4144d32ed7587b55bd4eb
Diego Rezende <diego@DESKTOP-LD0UU0S.localdomain>
2022-05-16 18:33:08 -0300

    ZKU Cohort 3 - Assignment 1 / Answers - Diego Rezende2

--- Part1/circuits/MerkleTree.circom
+++ Part1/circuits/MerkleTree.circom
@@ -1,13 +1,88 @@
 pragma circom 2.0.0;
 
 include "../node_modules/circomlib/circuits/poseidon.circom";
+//Included to compute MerkleTreeInclusionProof(n)
+include "../node_modules/circomlib/circuits/bitify.circom";
+include "../node_modules/circomlib/circuits/switcher.circom";
 
+//The solution is inspired by Tornado ( file merkleTree.circom )
+//Helper Template to calculate hash of items 2 by 2 
+//and throw an output array with the hashed items of the layer  
+template LayerHasher(depth){
+    //Declare variable tha guides the number of items per layer
+    var numOfLeaves = 2**depth;
+    
+    //Declare array input signals of hashed items 
+    //that will be taken and hashed 2 by 2 for next Tree layer
+    signal input inHashedItems[numOfLeaves * 2];
+    
+    //Declare array output signals that holds the computed hashed 
+    //items of the inserted depth Tree layer
+    signal output outHashedItems[numOfLeaves];
+    
+    //Declare component hash array of numOfLeaves items
+    //where each  position will be an instantiation of Poseidon 
+    //template with two inputs and its out will then be stored
+    //at outHashedItems array
+    component hash[numOfLeaves];
+    
+    //Loop to compute hashes 2by2 items and fill outHashedItems array with it
+    for(var i = 0; i < numOfLeaves; i++) {
+      hash[i] = Poseidon(2);
+      hash[i].inputs[0] <== inHashedItems[i * 2];
+      hash[i].inputs[1] <== inHashedItems[i * 2 + 1];
+      hash[i].out ==> outHashedItems[i];
+    }
+ 
+}
+
+//The solution is inspired by Tornado ( file merkleTree.circom )
 template CheckRoot(n) { // compute the root of a MerkleTree of n Levels 
     signal input leaves[2**n];
     signal output root;
-
     //[assignment] insert your code here to calculate the Merkle root from 2^n leaves
+    
+    //Declare component treeLayers array of size n (representing each tree layers ) 
+    //in which every layer is an instatiation of template LayerHasher where
+    //outHashedItems represents the hashed items within it  
+    component treeLayers[n];
+    
+    //Loop to navigate tree per levels from farthest up to its root
+    for(var level = n - 1; level >= 0; level--) {
+       
+       //Instantiation of component treeLayers in specifc level to 
+       //its LayerHasher template with same level
+       treeLayers[level] = LayerHasher(level);
+       
+       //Loop to populate the input (inHashedItems) from component treeLayers
+       for(var i = 0; i < (1 << (level + 1)); i++) {
+          
+          //Fill Every item from treeLayers input inHashedItems with initial leaves
+          //array input items if we are computing treeLayers[level n-1] otherwise
+          //fill the inHashedItems with the outHashedItems of previous computed hashed layer
+          treeLayers[level].inHashedItems[i] <== level == n - 1 ? leaves[i] : treeLayers[level + 1].outHashedItems[i];
+        }
+    }
+
+    //Boolean Conditional where in an existing Merkle Tree with n>0
+    //root receives the value of treeLayers[0].outHashedItems[0]
+    //Otherwise, receives the leaves[0] unique item
+    root <== n > 0 ? treeLayers[0].outHashedItems[0] : leaves[0];
+     
 }
+
+
+
+//The solution is inspired by Tornado ( file merkleProof.circom )
+//It also demonstrates how to use switcher.circom
+
+/*
+    Assume sel is binary.
+
+    If sel == 0 then outL = L and outR=R
+    If sel == 1 then outL = R and outR=L
+
+ */
 
 template MerkleTreeInclusionProof(n) {
     signal input leaf;
@@ -16,4 +91,31 @@ template MerkleTreeInclusionProof(n) {
     signal output root; // note that this is an OUTPUT signal
 
     //[assignment] insert your code here to compute the root from a leaf and elements along the path
+    
+    component switcher[n];
+    component hasher[n];
+    component indexBits[n];
+    
+    //Loop to fill the component indexBits array with the direction 
+    //Left(0) or Right(1) index in binary form of n bits
+    for( var index = 0; index<n; index++){
+        indexBits[index]= Num2Bits(4);
+        indexBits[index].in <== path_index[index];
+    }
+   
+    //Loop to use component switcher to position proval elements
+    //acording its path direction index and component hasher that
+    //will compute 2 by 2 hashes up to the proof root 
+    for (var i = 0; i < n; i++) {
+        switcher[i] = Switcher();
+        switcher[i].L <== i == 0 ? leaf : hasher[i - 1].out;
+        switcher[i].R <== path_elements[i];
+        switcher[i].sel <== indexBits[i].out[i];
+
+        hasher[i] = Poseidon(2);
+        hasher[i].inputs[0] <== switcher[i].outL;
+        hasher[i].inputs[1] <== switcher[i].outR;
+    }
+
+    root <== hasher[n - 1].out;
 }
--- Part1/contracts/MerkleTree.sol
+++ Part1/contracts/MerkleTree.sol
@@ -4,17 +4,116 @@ pragma solidity ^0.8.0;
 import { PoseidonT3 } from "./Poseidon.sol"; //an existing library to perform Poseidon hash on solidity
 import "./verifier.sol"; //inherits with the MerkleTreeInclusionProof verifier contract
 
+/**
+        __________           _____________________
+         indexing                 Merkle Tree 
+        convention                  3 levels   
+        ***********          *********************
+
+          3--->                   #### 14 ####  ---> ROOT
+                                 /            \
+          2--->                12              13
+                            /      \        /      \
+          1--->            08      09      10      11
+                          /  \    /  \    /  \    /  \
+          0--->          00  01  02  03  04  05  06  07  ---> 8 initial leaves  
+
+         Flattened Tree Layer must be initialized with zeroes and look like below:
+         hashes = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+                   |  |  |  |  |  |  |  |  |  |  |  |  |  |  |
+                   0  1  2  3  4  5  6  7  8  9  10 11 12 13 14
+         */
+
 contract MerkleTree is Verifier {
     uint256[] public hashes; // the Merkle tree in flattened array form
     uint256 public index = 0; // the current index of the first unfilled leaf
     uint256 public root; // the current Merkle root
+    
 
     constructor() {
         // [assignment] initialize a Merkle tree of 8 with blank leaves
+        //Initializing hashes to 15 positions 
+        hashes = new uint256[](15);
+        
+        uint256 exp = index/uint256(2);
+
+        //Filling layer 3
+        for(uint i=0; i<8; i++){
+            hashes[i] = 0;
+        }
+        //Filling layer 2
+        for(uint i = 0; i<=6; i+=2){
+            hashes[7+(2**exp)] = PoseidonT3.poseidon([hashes[i],hashes[i+1]]);
+        }
+        //Filling layer 1
+        for(uint i = 8; i<=10; i+=2){
+            hashes[11+(2**(exp/uint256(2)))] = PoseidonT3.poseidon([hashes[i],hashes[i+1]]);
+        }
+        //Filling First Root
+        hashes[14] = PoseidonT3.poseidon([hashes[12],hashes[13]]);
     }
 
+    
     function insertLeaf(uint256 hashedLeaf) public returns (uint256) {
         // [assignment] insert a hashed leaf into the Merkle tree
+        require(index<8,"Merkle Tree Initial Leaves Limit complete");
+        
+        hashes[index] = hashedLeaf;
+        uint256 exp = index/uint256(2);
+        
+        for(uint i=index; i<15 ; i++){
+            // Is even?
+            if(index % 2 == 0){
+                   hashes[7+(2**exp)] = PoseidonT3.poseidon([hashes[index], hashes[index+1]]);
+                   // Is even?
+                   if((7+(2**exp)) % 2 == 0){
+                            hashes[11+(2**(exp/uint256(2)))] = PoseidonT3.poseidon([hashes[(7+(2**exp))], hashes[(7+(2**exp))+1]]);
+                            // Is even?
+                            if((11+(2**(exp/uint256(2)))) % 2 == 0){
+                                hashes[14] = PoseidonT3.poseidon([hashes[(11+(2**(exp/uint256(2))))], hashes[(11+(2**(exp/uint256(2))))+1]]);
+                                root = hashes[14];
+                            }else{//odd path
+                                hashes[14] = PoseidonT3.poseidon([hashes[(11+(2**(exp/uint256(2))))-1], hashes[(11+(2**(exp/uint256(2))))]]);
+                                root = hashes[14];
+                            }                        
+                    }else{//odd path/////////////////////////
+                            hashes[11+(2**(exp/uint256(2)))] = PoseidonT3.poseidon([ hashes[(7+(2**exp))-1],hashes[(7+(2**exp))]]);
+                            // Is even?
+                            if((11+(2**(exp/uint256(2)))) % 2 == 0){
+                                hashes[14] = PoseidonT3.poseidon([hashes[(11+(2**(exp/uint256(2))))], hashes[(11+(2**(exp/uint256(2))))+1]]);
+                                root = hashes[14];
+                            }else{//odd path/////////////////////////
+                                hashes[14] = PoseidonT3.poseidon([hashes[(11+(2**(exp/uint256(2))))-1], hashes[(11+(2**(exp/uint256(2))))]]);
+                                root = hashes[14];
+                            }     
+                    }
+            }else{//odd path/////////////////////////
+                   hashes[7+(2**exp)] = PoseidonT3.poseidon([hashes[index-1], hashes[index]]);
+                   // Is even?
+                   if((7+(2**exp)) % 2 == 0){
+                            hashes[11+(2**(exp/uint256(2)))] = PoseidonT3.poseidon([hashes[(7+(2**exp))], hashes[(7+(2**exp))+1]]);
+                            // Is even?
+                            if((11+(2**(exp/uint256(2)))) % 2 == 0){
+                                hashes[14] = PoseidonT3.poseidon([hashes[(11+(2**(exp/uint256(2))))], hashes[(11+(2**(exp/uint256(2))))+1]]);
+                                root = hashes[14];
+                            }else{//odd path
+                                hashes[14] = PoseidonT3.poseidon([hashes[(11+(2**(exp/uint256(2))))-1], hashes[(11+(2**(exp/uint256(2))))]]);
+                                root = hashes[14];
+                            }                        
+                    }else{//odd path/////////////////////////
+                            hashes[11+(2**(exp/uint256(2)))] = PoseidonT3.poseidon([ hashes[(7+(2**exp))-1],hashes[(7+(2**exp))]]);
+                            // Is even?
+                            if((11+(2**(exp/uint256(2)))) % 2 == 0){
+                                hashes[14] = PoseidonT3.poseidon([hashes[(11+(2**(exp/uint256(2))))], hashes[(11+(2**(exp/uint256(2))))+1]]);
+                                root = hashes[14];
+                            }else{//odd path/////////////////////////
+                                hashes[14] = PoseidonT3.poseidon([hashes[(11+(2**(exp/uint256(2))))-1], hashes[(11+(2**(exp/uint256(2))))]]);
+                                root = hashes[14];
+                            }     
+                    }            
+            }
+        }        
+        return root;
     }
 
     function verify(
@@ -25,5 +124,7 @@ contract MerkleTree is Verifier {
         ) public view returns (bool) {
 
         // [assignment] verify an inclusion proof and check that the proof root matches current root
+        // Returning function verifyProof from Verifier contract 
+        return verifyProof(a, b, c, input);
     }
 }
